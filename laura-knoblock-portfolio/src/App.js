0af51a02ab369a409a8ef9ca2855996422b716c3
Your Name <you@example.com>
2023-10-04 12:17:03 -0500

    Fun moving graphic, fun hover for text added

---
+++ laura-knoblock-portfolio/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={process.env.PUBLIC_URL + '/Logo.png'} className="App-logo" alt="logo" />
+        <h1 class="grow">
+          Laura Knoblock
+        </h1>
+      
+        
+      </header>
+     
+    </div>
+  );
+}
+
+export default App;
